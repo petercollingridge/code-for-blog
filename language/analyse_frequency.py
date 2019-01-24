d7c55e6c08ed91f28d9d85a0cff3e8b594068499
Peter Collingridge <pcollingridge@pcolling-mac.oxfordnanolabs.local>
2019-01-24 12:25:43 +0000

    Function to get_length_distribution

--- language/analyse_frequency.py
+++ language/analyse_frequency.py
@@ -1,16 +1,33 @@
 import os
 from utils import get_word_counts
+from collections import defaultdict
 
 
-def show_most_common_words(word_counts, num=10):
+def get_most_common_words(word_counts, num=10):
+    common_word_counts = []
+
     for i, (word, count) in enumerate(sorted(word_counts.items(), key=lambda word: -word[1])):
-        print(word, count)
+        common_word_counts.append((word, count))
         if (i == num - 1):
             break
+
+    return common_word_counts
+
+
+def get_length_distribution(word_counts):
+    length_distribution = defaultdict(int)
+
+    for word, count in word_counts.items():
+        length_distribution[len(word)] += count
+
+    return length_distribution
 
 
 if __name__ == '__main__':
     import os
     word_counts = get_word_counts(os.path.join('word_lists', 'filtered_word_counts.txt'))
-    show_most_common_words(word_counts)
-    print(word_counts['be'])
+
+    common_word_counts = get_most_common_words(word_counts)
+    length_distribution = get_length_distribution(word_counts)
+    print(length_distribution)
+
