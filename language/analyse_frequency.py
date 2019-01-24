020be56b06bb3d5dcb81f4c4f42570fa85472522
Peter Collingridge <pcollingridge@pcolling-mac.oxfordnanolabs.local>
2019-01-24 12:40:48 +0000

    Functions to generate letter frequencies and print them in order

--- language/analyse_frequency.py
+++ language/analyse_frequency.py
@@ -23,11 +23,39 @@ def get_length_distribution(word_counts):
     return length_distribution
 
 
+def get_letter_counts(word_counts):
+    letter_counts = defaultdict(int)
+
+    for word, count in word_counts.items():
+        for letter in word:
+            letter_counts[letter] += count
+
+    return letter_counts
+
+
+def convert_to_percentage(counts):
+    total = sum(counts.values())
+    percentages = dict()
+
+    for item, count in counts.items():
+        percentages[item] = 100 * count / total
+    
+    return percentages
+
+
+def show_in_order(counts):
+    for item in sorted(counts.items(), key=lambda item: -item[1]):
+        print(item)
+
+
 if __name__ == '__main__':
     import os
     word_counts = get_word_counts(os.path.join('word_lists', 'filtered_word_counts.txt'))
 
-    common_word_counts = get_most_common_words(word_counts)
+    # common_word_counts = get_most_common_words(word_counts)
     length_distribution = get_length_distribution(word_counts)
-    print(length_distribution)
+
+    letter_counts = get_letter_counts(word_counts)
+
+    show_in_order(letter_counts)
 
