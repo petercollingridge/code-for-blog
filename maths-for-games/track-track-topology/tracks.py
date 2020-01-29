a8de4f0ab383f19c4476c08329ca0bcbe40f3a2c
Peter Collingridge <peter.collingridge@gmail.com>
2020-01-29 21:27:00 +0000

    Get working track

--- maths-for-games/track-track-topology/tracks.py
+++ maths-for-games/track-track-topology/tracks.py
@@ -1,72 +1,75 @@
 from collections import namedtuple
+from enum import IntEnum
 
 
 Connection = namedtuple('Connection', ['point', 'position'])
-ARM1 = 0
-ARM2 = 0
-BASE = 2
+
+class Position(IntEnum):
+    ARM1 = 0
+    ARM2 = 1
+    BASE = 2
 
 class System:
     def __init__(self, num_points=0):
         self.points = []
-        self.tracks = []
         self.add_points(num_points)
 
     def add_points(self, num_points):
         for _ in range(num_points):
-            self.points.append(Point())
+            n = len(self.points)
+            self.points.append(Point(n))
 
     def add_track(self, point_index1, position1, point_index2, position2):
-        point1 = self.points[point_index1]
-        point2 = self.points[point_index2]
+        connection1 = self._get_connection(point_index1, position1)
+        connection2 = self._get_connection(point_index2, position2)
 
         # Create connections
-        connection1 = Connection(point1, position1)
-        connection2 = Connection(point2, position2)
+        connection1.point.connections[position1] = connection2
+        connection2.point.connections[position2] = connection1
 
-        track_index = len(self.tracks)
-        track = Track(connection1, connection2, track_index)
-        point1.tracks[position1] = track
-        point2.tracks[position2] = track
-        self.tracks.append(track)
+    def _get_connection(self, index, position):
+        point = self.points[index]
+        return Connection(point, position)
+
+    def run(self, point_index, position, n):
+        connection = self._get_connection(point_index, position)
+        
+        for _ in range(n):
+            print("{0}: {1}".format(
+                connection.point,
+                str(connection.position)
+            ))
+            connection = connection.point.next(connection.position)
 
 
 class Point:
-    def __init__(self):
-        self.tracks = [None] * 3
+    def __init__(self, index):
+        self.index = index
+        self.connections = [None] * 3
         self.switch = 0
 
     def next(self, position):
-        if position == BASE:
-            # TODO
-            pass
+        """
+        Given a position representing the point at which a train enters, the point,
+        return the connection to the next point and position.
+        """
+        if position == Position.BASE:
+            # Entering the point at the base, so leave via an arm
+            # determined by the switch
+            return self.connections[self.switch]
         else:
-            outgoing_track = self.tracks[BASE]
-            return outgoing_track.next()
-
-
-class Track:
-    def __init__(self, connection1, connection2, index):
-        self.connections = [connection1, connection2]
-        self.index = index
-
-    def next(self, connection):
-        """ Given one connection, return the connection at the other end of the track. """
-
-        index = self.connections.index(connection)
-        if index == 0:
-            return self.connections[1]
-        elif index == 1:
-            return self.connections[0]
-        else:
-            raise IndexError
+            # Entering the point at an arm, so leave via the base
+            # The switch changes the point to this arm
+            self.switch = position
+            return self.connections[Position.BASE]
 
     def __repr__(self):
-        return "Track {0}".format(self.index)
-
+        return "Point {}".format(self.index)
 
 if __name__ == '__main__':
     system = System(2)
-    system.add_track(0, BASE, 1, BASE)
-    system.add_track(0, ARM1, 0, ARM2)
-    system.add_track(1, ARM1, 1, ARM2)
+    system.add_track(0, Position.BASE, 1, Position.BASE)
+    system.add_track(0, Position.ARM1, 0, Position.ARM2)
+    system.add_track(1, Position.ARM1, 1, Position.ARM2)
+    system.run(0, Position.BASE, 10)
+
