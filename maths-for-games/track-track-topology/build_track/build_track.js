f04f15bf73ccacb0eca6168f662d1edbdbad57ef
Peter Collingridge <peter.collingridge@gmail.com>
2020-02-07 23:22:59 +0000

    Method to add connections

--- maths-for-games/track-track-topology/build_track/build_track.js
+++ maths-for-games/track-track-topology/build_track/build_track.js
@@ -5,11 +5,35 @@ var vm = new Vue({
             { x: 100, y: 100, angle: 270 },
             { x: 300, y: 100, angle: 90 },
         ],
-        selectedPoint: false
+        connections: [],
+        selectedPoint: false,
+        connectionPoint1: '',
+        connectionPoint2: '',
+        connectionPosition1: '',
+        connectionPosition2: '',
+    },
+    computed: {
+        canAddConnections: function() {
+            return this.connectionPoint1 && this.connectionPoint2 &&
+                this.connectionPoint1 && this.connectionPoint2;
+        }
     },
     methods: {
         selectPoint: function(i) {
             this.selectedPoint = i;
+        },
+        addConnection() {
+            // Check is connection is valid
+
+            this.connections.push({
+                point1: this.connectionPoint1,
+                point2: this.connectionPoint2,
+                position1: this.connectionPosition1,
+                position2: this.connectionPosition2,
+            })
+        },
+        getConnectionPath(connection) {
+            co
         }
     }
 });
