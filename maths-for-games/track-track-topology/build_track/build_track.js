20dac7cba3742329d415a0aa49026605e558e0a4
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-02-10 21:37:52 +0000

    Change angle of track

--- maths-for-games/track-track-topology/build_track/build_track.js
+++ maths-for-games/track-track-topology/build_track/build_track.js
@@ -1,14 +1,15 @@
 const DEGREES_30 = Math.PI * 30 / 180;
 const POINT_ARC_LENGTH = 50;
-const POINT_SIZE = 2 * POINT_ARC_LENGTH * Math.sin(DEGREES_30 / 2);
+// const POINT_SIZE = 2 * POINT_ARC_LENGTH * Math.sin(DEGREES_30 / 2);
+const POINT_SIZE = POINT_ARC_LENGTH;
 
 
 var vm = new Vue({
     el: '#track-builder',
     data: {
         points: [
-            { x: 100, y: 100, angle: 270 },
-            { x: 300, y: 100, angle: 90 },
+            { x: 100, y: 100, angle: 0 },
+            { x: 300, y: 100, angle: 180 },
         ],
         connections: [],
         selectedPoint: false,
@@ -40,9 +41,10 @@ var vm = new Vue({
             })
         },
         getConnectionPath({point1, point2, position1, position2,}) {
+            const p = this.points[point1];
             const coord1 = this.getCoordinates(position1, this.points[point1]);
             const coord2 = this.getCoordinates(position2, this.points[point2]);
-            return `M${ coord1.x } ${ coord1.y }L${ coord2.x } ${ coord2.y }`;
+            return `M${p.x} ${p.y} ${ coord1.x } ${ coord1.y }L${ coord2.x } ${ coord2.y }`;
         },
         getCoordinates(position, point) {
             const { x, y, angle } = point;
