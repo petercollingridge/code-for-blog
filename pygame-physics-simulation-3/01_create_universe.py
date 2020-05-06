8c8e2d35052c97843a7814a3eb33b4652b6ace51
Peter Collingridge <peter.collingridge@nanoporetech.com>
2020-05-06 21:58:06 +0100

    Start a Python3 pygame physics tutorial

--- pygame-physics-simulation-3/01_create_universe.py
+++ pygame-physics-simulation-3/01_create_universe.py
@@ -1,15 +1,19 @@
 import pygame
 
+# Universe constants
 BACKGROUND_COLOR = (255, 255, 255)
 (WIDTH, HEIGHT) = (300, 200)
 
+# Create a Pygame window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Tutorial 1')
+
+# Display a blank screen
 screen.fill(BACKGROUND_COLOR)
 pygame.display.flip()
 
-running = True
-while running:
+# Keep showing the window until the user closes it
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit(); 
--- pygame-physics-simulation-3/02_create_particle.py
+++ pygame-physics-simulation-3/02_create_particle.py
@@ -1,15 +1,27 @@
 import pygame
+pygame.init()
 
+# Universe constants
 BACKGROUND_COLOR = (255, 255, 255)
 (WIDTH, HEIGHT) = (300, 200)
 
+# Create a Pygame window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Tutorial 1')
+pygame.display.set_caption('Tutorial 2')
+
+# Display a blank screen with a particle on it
 screen.fill(BACKGROUND_COLOR)
+
+particle_colour = (0, 0, 255)
+particle_position = (50, 30)
+particle_size = 20
+particle_thickness = 0
+
+pygame.draw.circle(screen, particle_colour, particle_position, particle_size, particle_thickness)
 pygame.display.flip()
 
-running = True
-while running:
+# Keep showing the window until the user closes it
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
