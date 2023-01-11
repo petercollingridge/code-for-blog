499d98e5e2b1978fb2b7cb8bde5455936ce05392
Peter Collingridge <peter.collingridge@nanoporetech.com>
2023-01-11 19:12:49 +0000

    Add simulated combate

--- maths-for-games/heroquest/combat.py
+++ maths-for-games/heroquest/combat.py
@@ -1,3 +1,4 @@
+import random
 import operator as op
 from collections import defaultdict
 from fractions import Fraction
@@ -30,6 +31,54 @@ class Character():
             p = n_choose_r(dice, n) * (probability ** n) * ((1 - probability) ** (dice - n))
             probabilities.append((n, p))
         return probabilities
+
+    def simulate_combat_probabilities(self, other, n):
+        """
+        Simulation fighting another character n times
+        and return the distribution of this character's
+        body points at the end of combat.
+        """
+        final_body_points = defaultdict(int)
+
+        for _ in range(n):
+            body_points = self.simulate_combat(other)
+            final_body_points[body_points] += 1
+
+        # Convert counts to probabilities
+        for body in final_body_points:
+            final_body_points[body] /= n
+
+        return final_body_points
+
+    def simulate_combat(self, other):
+        self_body = self.body
+        other_body = other.body
+        self_defense_p = float(self._get_defense_prob())
+        other_defense_p = float(other._get_defense_prob())
+
+        # Each loop is one round of combat
+        while True:
+            # Self attacks other
+            attack = sum(random.random() < 0.5 for _ in range(self.attack))
+            defend = sum(random.random() < other_defense_p for _ in range(other.defend))
+            damage = max(0, attack - defend)
+            other_body -= damage
+            if other_body <= 0:
+                return self_body
+
+            # Other attacks self
+            attack = sum(random.random() < 0.5 for _ in range(other.attack))
+            defend = sum(random.random() < self_defense_p for _ in range(self.defend))
+            damage = max(0, attack - defend)
+            self_body -= damage
+            if self_body <= 0:
+                return 0
+
+    def simulate_attack(self, other):
+        attack = sum(random.random() < 0.5 for _ in range(self.attack))
+        defend = sum(random.random() < other._get_defense_prob() for _ in range(other.defend))
+        return max(0, attack - defend)
+
 
 
 class Hero(Character):
@@ -93,24 +142,28 @@ def get_prob_dist_for_damage_on_leaving_combat_state(character1, character2):
 def get_probability_distribution_for_final_damage(character1, character2):
     p_damage = get_prob_dist_for_damage_on_leaving_combat_state(character1, character2)
 
-    # Probability of entering combat state of having x body points
-    # There is a 100% probability of entering with the starting body points
-    p_combat = {character1.body : 1}
-    p_final_damage = [p_damage[0]]
+    # Map number of bps to probability of entering combat with that num of body points
+    p_combat = defaultdict(int)
 
-    for body in range(character1.body - 1, -1, -1):
-        # Probability of reaching this combat state
-        p = 0
+    # There is a 100% probability of entering with the starting body points
+    p_combat[character1.body] = 1
+
+    for body in range(character1.body, 0, -1):
+        p_this_state = p_combat[body]
+
         for damage in range(1, character2.attack + 1):
-            p += p_combat.get(body + damage, 0) * p_damage[damage]
-        p_combat[body] = p
-        p_final_damage.append(p * p_damage[0])
+            new_body = max(0, body - damage)
+            p_combat[new_body] += p_this_state * p_damage[damage]
+
+    p_final_damage = [p_combat[bp] * p_damage[0] for bp in range(character1.body + 1)]
+    p_final_damage.reverse()
 
     print(f"p(Monster wins) = 1 in {float(1 / p_combat[0])}")
     print(f"E(damage to hero) = {float(get_expected_value(p_final_damage))}")
 
     print(float(p_combat[0]))
-    print([float(d) for d in p_final_damage])
+    for i, damage in enumerate(p_final_damage):
+        print(character1.body - i, float(damage))
     print(float(sum(p_final_damage)))
 
 
@@ -143,7 +196,9 @@ gargoyle = Monster(4, 4, 1)
 # print(attack(barbarian, gargoyle))
 # print(get_prob_of_0_damage(barbarian, goblin))
 
-# get_probability_distribution_for_final_damage(barbarian, goblin)
 # get_probability_distribution_for_final_damage(barbarian, gargoyle)
 # get_probability_distribution_for_final_damage(wizard, goblin)
-get_probability_distribution_for_final_damage(wizard, gargoyle)
+# get_probability_distribution_for_final_damage(wizard, gargoyle)
+
+get_probability_distribution_for_final_damage(barbarian, goblin)
+# print(barbarian.simulate_combat_probabilities(goblin, 1000000))
